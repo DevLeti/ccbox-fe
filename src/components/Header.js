508746101bb6ae4abbd108f7656fbbcd1a341e9f
DevLeti <devleti0106@gmail.com>
2022-04-28 23:21:07 +0900

    EDIT: make component - login, register

--- src/components/Header.js
+++ src/components/Header.js
@@ -9,6 +9,24 @@ import {
 import logo from "../assets/logo.png";
 import "./Header.css";
 // import React from "react";
+function Login() {
+  return (
+    <Nav.Link href="login" className="rounded bg-secondary text-white m-1 px-3">
+      로그인
+    </Nav.Link>
+  );
+}
+function Register() {
+  return (
+    <Nav.Link
+      href="register"
+      eventKey="1"
+      className="rounded bg-primary text-white m-1 px-3"
+    >
+      회원가입
+    </Nav.Link>
+  );
+}
 export function Header() {
   return (
     // py-0~5 높이 조절
@@ -27,19 +45,8 @@ export function Header() {
       <Navbar.Collapse id="basic-navbar-nav">
         {/* ms-auto: 우측, me-auto: 좌측 정렬 */}
         <Nav className="ms-auto">
-          <Nav.Link
-            href="login"
-            className="rounded bg-secondary text-white m-1 px-3"
-          >
-            로그인
-          </Nav.Link>
-          <Nav.Link
-            href="register"
-            eventKey="1"
-            className="rounded bg-primary text-white m-1 px-3"
-          >
-            회원가입
-          </Nav.Link>
+          <Login />
+          <Register />
           {/* <NavDropdown title="Dropdown" id="basic-nav-dropdown">
             <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
             <NavDropdown.Item href="#action/3.2">
