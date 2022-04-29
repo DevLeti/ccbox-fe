a0e42068797dfa2218f778cb83c75459644f9fd0
DevLeti <devleti0106@gmail.com>
2022-04-29 20:13:42 +0900

    Restore App.js to test main content component

--- src/index.js
+++ src/index.js
@@ -4,12 +4,13 @@ import "./index.css";
 import reportWebVitals from "./reportWebVitals";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { Header } from "./components/Header";
+import App from "./pages/App/App";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <Header />
-    {/* <App /> */}
+    <App />
   </React.StrictMode>
 );
 
